986540a9b82c0dba85f4bb2415fe1041deb39d87
OTKRyu <appn12@gmail.com>
2022-03-23 12:02:20 +0900

    fix: fix turn bug

--- client/src/views/Match.vue
+++ client/src/views/Match.vue
@@ -409,30 +409,32 @@ export default {
       }
     }, 1000);
     document.addEventListener("keyup", (e) => {
-      let pressedKey = String(e.key);
-      this.error = false;
-      this.errorMsg = "";
-      if (pressedKey === "Backspace" && this.nLetter !== 0) {
-        this.deleteLetter();
-        return;
-      }
-      if (pressedKey === "Enter") {
-        this.submit();
-        return;
-      }
-      if (
-        pressedKey.length === 1 &&
-        pressedKey.charCodeAt(0) > 96 &&
-        pressedKey.charCodeAt(0) < 123
-      ) {
-        this.insertLetter(pressedKey);
-      }
-      if (
-        pressedKey.length === 1 &&
-        pressedKey.charCodeAt(0) > 64 &&
-        pressedKey.charCodeAt(0) < 91
-      ) {
-        this.insertLetter(pressedKey);
+      if (this.myTurn === true) {
+        let pressedKey = String(e.key);
+        this.error = false;
+        this.errorMsg = "";
+        if (pressedKey === "Backspace" && this.nLetter !== 0) {
+          this.deleteLetter();
+          return;
+        }
+        if (pressedKey === "Enter") {
+          this.submit();
+          return;
+        }
+        if (
+          pressedKey.length === 1 &&
+          pressedKey.charCodeAt(0) > 96 &&
+          pressedKey.charCodeAt(0) < 123
+        ) {
+          this.insertLetter(pressedKey);
+        }
+        if (
+          pressedKey.length === 1 &&
+          pressedKey.charCodeAt(0) > 64 &&
+          pressedKey.charCodeAt(0) < 91
+        ) {
+          this.insertLetter(pressedKey);
+        }
       }
     });
   },
